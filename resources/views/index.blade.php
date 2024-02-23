3349e647b635ab2dd55f6a11584672cc42eb7a65
maman27.dev@gmail.com <rohman.banana27@gmail.com>
2024-02-24 05:33:22 +0700

    laravel bootcamp batch 2

---
+++ resources/views/index.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+    <title>Laravel</title>
+</head>
+
+<body>
+    <div class="container mt-5">
+        <div>
+            <h3 class="mb-4">Create Task</h3>
+            <div class="row">
+                <div class="col-md-6">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th style="width: 1%" class="text-center" scope="col">No</th>
+                                <th scope="col" class="text-center">Name</th>
+                                <th scope="col" class="text-center">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th class="text-center" scope="row">1</th>
+                                <td class="text-center">Ngobar</td>
+                                <td class="text-center">#</td>
+                            </tr>
+                            <tr>
+                                <th class="text-center" scope="row">2</th>
+                                <td class="text-center">Makan</td>
+                                <td class="text-center">#</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="mb-3">
+                                <label for="exampleFormControlInput1" class="form-label">Name</label>
+                                <input type="email" class="form-control" id="exampleFormControlInput1"
+                                    placeholder="tulis task harianmu">
+                            </div>
+                            <button type="button" class="btn btn-primary">Add Task</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
