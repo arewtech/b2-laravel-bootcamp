6eedf5e8e686cbfa17965fa68f0817b15ee9b6c8
maman27.dev@gmail.com <rohman.banana27@gmail.com>
2024-03-14 21:42:05 +0700

    stfu bye

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,21 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-    <title>Laravel</title>
-</head>
-
-<body>
-    <div class="container mt-5">
+@extends('layouts.app')
+@section('content')
+    <div class="container-fluid">
         <div>
             <h3 class="mb-4">Create Task</h3>
             <div class="row">
@@ -81,6 +66,4 @@
             </div>
         </div>
     </div>
-</body>
-
-</html>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,11 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('tasks', TaskController::class);
+Route::get('/', function () {
+    return view('dashboard.index');
+})->name('dashboard');
+Route::resource('/tasks', TaskController::class);
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
